1c4684bb428d937550c4cdcabe9b8ecc1469cc45
daryle <daryle.tan@gmail.com>
2023-11-13 21:13:07 -0600

    added more functions

--- src/test/Blackjack.sol
+++ src/test/Blackjack.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20
+pragma solidity ^0.8.20;
 
-contract Blackjack {
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract Blackjack is VRFConsumerBaseV2 {
 // create a variable of all 52 cards
 enum Suit { Hearts, Diamonds, Clubs, Spades };
 enum Rank { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
@@ -14,6 +18,10 @@ struct Card {
 mapping(uint8 => Card) public deck;
 // implement the VRF to randomly select cards from the deck
 // create function to start game
+function startGame() external {}
 // create logic to call bust or win
+function bustOrBlackjack() internal {}
 // create a function to hit
+function hitCard() external {}
+
 }
