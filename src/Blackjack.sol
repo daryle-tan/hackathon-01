22e86b6a77232860a95d8bb17a93e614541bb73c
daryle <daryle.tan@gmail.com>
2023-11-17 22:30:34 -0600

    updated struct

--- src/Blackjack.sol
+++ src/Blackjack.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 import "forge-std/console.sol";
 
 contract Blackjack is VRFConsumerBaseV2 {
-    // create a variable of all 52 cards
+    VRFCoordinatorV2Interface immutable COORDINATOR;
+    LinkTokenInterface immutable LINKTOKEN;
+    // create variables of all 52 cards
     enum Suit {
         Hearts,
         Diamonds,
@@ -34,19 +36,18 @@ contract Blackjack is VRFConsumerBaseV2 {
     struct Card {
         Suit suit;
         Rank rank;
+        uint8 cardValue;
     }
-    VRFCoordinatorV2Interface immutable COORDINATOR;
-    LinkTokenInterface immutable LINKTOKEN;
 
     // Your subscription ID.
     uint64 immutable s_subscriptionId;
     bytes32 immutable s_keyHash;
-    uint32 immutable s_callbackGasLimit = 100000;
+    uint32 immutable s_callbackGasLimit;
 
     // The default is 3, but you can set this higher.
-    uint16 immutable s_requestConfirmations = 3;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
 
-    uint8 public immutable s_numCards = 2;
+    uint8 internal s_numCards = 4;
     uint256 internal s_randomResult;
     uint256 internal desiredRange = 52;
     uint256 public s_requestId;
@@ -59,24 +60,26 @@ contract Blackjack is VRFConsumerBaseV2 {
     }
 
     constructor(
-        uint64 subscriptionId,
         address vrfCoordinator,
         address link,
-        bytes32 keyHash
+        bytes32 keyHash,
+        uint32 callbackGasLimit,
+        uint64 subscriptionId
     ) VRFConsumerBaseV2(vrfCoordinator) {
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
         LINKTOKEN = LinkTokenInterface(link);
         s_keyHash = keyHash;
         s_owner = msg.sender;
+        s_callbackGasLimit = callbackGasLimit;
         s_subscriptionId = subscriptionId;
     }
 
-    function requestRandomWords() external onlyOwner {
+    function requestRandomWords() public onlyOwner {
         // Will revert if subscription is not set and funded.
         s_requestId = COORDINATOR.requestRandomWords(
-            s_keyHash,
+            s_keyHash, // gaslane
             s_subscriptionId,
-            s_requestConfirmations,
+            REQUEST_CONFIRMATIONS,
             s_callbackGasLimit,
             s_numCards
         );
@@ -92,14 +95,18 @@ contract Blackjack is VRFConsumerBaseV2 {
     }
 
     // create function to start game
-    function startGame() external {}
-
-    // create logic to call bust or win
-    function bustOrBlackjack() internal {}
+    function startGame() public {
+        // if s_numCards != 4 then set to 4
+    }
 
     // create a function to hit
-    function hitCard() external {}
+    function hitCard() public {
+        // set s_numCards to 1 and call requestRandomWords and fulfillRandomWords
+    }
 
     // create function for standing
     function standingHand() external {}
+
+    // create logic to bust or win
+    function bustOrBlackjack() internal {}
 }
