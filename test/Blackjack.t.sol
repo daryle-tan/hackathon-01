4feaeee6c96c5e58ad0e1dbf65cd7079f1d19c48
daryle <daryle.tan@gmail.com>
2024-01-06 16:20:27 -0800

    added baseFee and gasPriceLink

--- test/Blackjack.t.sol
+++ test/Blackjack.t.sol
@@ -6,6 +6,7 @@ import {HelperConfig} from "../script/HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "./mocks/MockVRFCoordinatorV2.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
 
 // import {CreateSubscription} from "../../script/Interactions.s.sol";
 
@@ -16,10 +17,13 @@ contract BlackjackTest is Test {
     // MockOracle public mockOracle;
     Blackjack public blackjack;
 
+    uint96 baseFee = 0.25 ether;
+    uint96 gasPriceLink = 1e9;
+
     uint64 subscriptionId;
     bytes32 gasLane;
     uint32 callbackGasLimit;
-    address vrfCoordinatorV2;
+    VRFCoordinatorV2Mock vrfCoordinatorV2;
     bool gameStarted;
     bool playerTurn;
     bool dealerTurn;
@@ -30,6 +34,7 @@ contract BlackjackTest is Test {
         // linkToken = new LinkToken();
         // mockOracle = new MockOracle(address(linkToken));
         blackjack = new Blackjack();
+        vrfCoordinatorV2 = new VRFCoordinatorV2Mock(baseFee, gasPriceLink);
     }
 
     function testStartGame() public {
@@ -44,10 +49,71 @@ contract BlackjackTest is Test {
 
     function testDealCard() public {
         blackjack.startGame();
-        blackjack.dealCards();
+        uint256 requestId = blackjack.dealCards();
+        assertTrue(requestId != 0);
         assertEq(cardsAlreadyDealt, true);
         assertEq(playerTurn, true);
     }
+
+    // function testFulfillRandomWords() public {
+    //     // ...same setup as above
+
+    //     blackjack.startGame();
+    //     uint256 requestId = blackjack.dealCards();
+    //     uint256[] memory randomWords = new uint256[](4);
+    //     randomWords[0] = uint256(
+    //         keccak256(
+    //             abi.encodePacked(
+    //                 block.timestamp,
+    //                 block.difficulty,
+    //                 address(this),
+    //                 1
+    //             )
+    //         )
+    //     );
+    //     randomWords[1] = uint256(
+    //         keccak256(
+    //             abi.encodePacked(
+    //                 block.timestamp,
+    //                 block.difficulty,
+    //                 address(this),
+    //                 2
+    //             )
+    //         )
+    //     );
+    //     randomWords[2] = uint256(
+    //         keccak256(
+    //             abi.encodePacked(
+    //                 block.timestamp,
+    //                 block.difficulty,
+    //                 address(this),
+    //                 3
+    //             )
+    //         )
+    //     );
+    //     randomWords[3] = uint256(
+    //         keccak256(
+    //             abi.encodePacked(
+    //                 block.timestamp,
+    //                 block.difficulty,
+    //                 address(this),
+    //                 4
+    //             )
+    //         )
+    //     );
+
+    //     // Simulate the VRFCoordinator calling fulfillRandomWords
+    //     blackjack.call(
+    //         abi.encodeWithSelector(
+    //             Blackjack.fulfillRandomWords.selector,
+    //             requestId,
+    //             randomWords
+    //         )
+    //     );
+
+    //     Blackjack.Card[] memory cards = blackjack.getRandomResult();
+    //     assertEq(cards.length, 4);
+    // }
 
     function testPlayerHitCard() public {
         blackjack.startGame();
