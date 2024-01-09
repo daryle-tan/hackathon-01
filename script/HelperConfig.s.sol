679ea8b4576a39d3dd82a013f8e638d70039344e
daryle <daryle.tan@gmail.com>
2024-01-08 20:48:34 -0600

    added getOrCreateAnvilEthConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -8,6 +8,14 @@ import {Script} from "forge-std/Script.sol";
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
 
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
     struct NetworkConfig {
         address vrfCoordinator;
         address link;
@@ -15,8 +23,6 @@ contract HelperConfig is Script {
         uint32 callbackGasLimit;
         uint64 subscriptionId;
         uint256 deployerKey;
-        VRFCoordinatorV2Interface immutable COORDINATOR = VRFCoordinatorV2Interface(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625);
-        LinkTokenInterface immutable LINKTOKEN = LinkTokenInterface(0x779877A7B0D9E8603169DdbD7836e478b4624789);
     }
 
     mapping(uint256 => NetworkConfig) public chainIdToNetworkConfig;
@@ -27,18 +33,22 @@ contract HelperConfig is Script {
 
     function getSepoliaEthConfig()
         public
-        returns (
-            // view
-            NetworkConfig memory sepoliaNetworkConfig
-        )
+        view
+        returns (NetworkConfig memory sepoliaNetworkConfig)
     {
         sepoliaNetworkConfig = NetworkConfig({
             vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
             link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
             keyHash: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
             callbackGasLimit: 2500000,
-            subscriptionId: 7200,
-            deployerKey: vm.envUint("PRIVATE_KEY")
+            subscriptionId: 7200
+            // deployerKey: vm.envUint("PRIVATE_KEY")
         });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
     }
 }
